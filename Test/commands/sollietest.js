ecfe6e3fde3f4a80e0a8457b0d03f6aec12da69a
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-23 12:38:21 +0100

    Update sollietest.js

--- Test/commands/sollietest.js
+++ Test/commands/sollietest.js
@@ -94,10 +94,8 @@ module.exports.run = async (client, message, args) => {
                         .setColor("#00BFFF")
                         .setDescription("Nog Vragen?");
 
-                    settedParrent.send(message.author.id);
-                    settedParrent.send(embedParent);
-                    settedParrent.send(vraag1);
-
+                        settedParrent.send(embedParent);
+                        settedParrent.send(vraag1);
      
     
                         settedParrent.awaitMessages(s => s.author.id == message.author.id, {max: 1}).then(antwoord => {
