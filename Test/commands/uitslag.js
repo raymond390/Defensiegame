e9789a72ae9f17226300555c298b4932eb8e623d
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-23 15:22:27 +0100

    fix

--- Test/commands/uitslag.js
+++ Test/commands/uitslag.js
@@ -57,7 +57,7 @@ module.exports.run = async (client, message, args) => {
 
             message.channel.send(redenEmbed).then(msg => msg.delete({ timeout: 10000}));
 
-            message.channel.await.awaitMessage(filter, {max:1, time: 10000}).then(collected => {
+            message.channel.await.awaitMessages(filter, {max:1, time: 10000}).then(collected => {
 
                 var redenBad = collected.first();;
                                              
