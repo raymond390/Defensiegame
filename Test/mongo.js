f418f9c5cacccdb145b5936a1c759802027ae744
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-12 19:06:50 +0100

    z

--- Test/mongo.js
+++ Test/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 // const { mongoPath } = require('./config.json')
 
-const mongoPath = 'mongodb+srv://raymond:s3MNTT9oKacrTeO3@raymonds.w37hc.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const mongoPath = 'mongodb+srv://raymond:s3MNTT9oKacrTeO3@raymonds.w37hc.mongodb.net/Defensie?retryWrites=true&w=majority'
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
